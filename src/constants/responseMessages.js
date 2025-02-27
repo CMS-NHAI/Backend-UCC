b8481eacc44e0c95380356700c54b18e2e2ec15e
Deepanshu Rajput <deepanshu_r@cms.co.in>
2025-02-27 17:45:20 +0530

    added changes

--- src/constants/responseMessages.js
+++ src/constants/responseMessages.js
@@ -4,15 +4,15 @@ export const RESPONSE_MESSAGES = {
       USER_ID_MISSING: 'UserId is required.',
       INVALIDFILTER: 'Invalid filter provided.',
       REQUEST_PROCESSING_ERROR: "Error Occured while processing request",
-      STRETCH_DATA_ERROR: "An error occurred while creating required stretch based on given start and end chainages."
+      STRETCH_DATA_ERROR: "An error occurred while creating required stretch based on given start and end chainages.",
+      FILE_NOT_FOUND: "File not found",
+      REQUEST_PROCESSING_ERROR: "Request processing error",
+      FILE_UPLOAD_FAILED: "File upload failed",
+      USER_NOT_FOUND: "User not found",
+      DOCUMENT_TYPE_NOT_FOUND : "Document type not provided",
+      INVALID_FILE_TYPE:"Invalid file type. Only kml files are allowed.",
     },
     SUCCESS: {
-        ANALYTICSFETCHED: 'Attendance analytics fetched successfully.',
-        NO_TEAM_MEMBERS: "No team members found to fetch data.",
-        NO_UCC_FOUND: "No UCCs found in the database for the given user.",
-        OUTSIDE_WORK_AREA: "You are out of your work area",
-        INSIDE_WORK_AREA: "You are within your work area",
-        ATTENDANCE_RECORDS_FETCHED_SUCCESSFULLY:'Attendance Count fetched Successfully',
-        NO_UCC_FOR_USERID: "No UCC found for the given user's userID."
+        FILE_UPLOADED: "File uploaded successfully",
     },
   }
--- src/services/uccService.js
+++ src/services/uccService.js
@@ -1,8 +1,61 @@
-import {findUserById} from './db/userDbService.js'
-export const getUserDetails = async (userId) => {
-  const user = await findUserById(userId);
-  if (!user) {
-    throw new Error('User not found');
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import {prisma} from '../config/prismaClient.js';
+import {upload} from '../helpers/multerConfig.js';
+import APIError from "../utils/apiError.js";
+import { RESPONSE_MESSAGES } from '../constants/responseMessages.js';
+import { STATUS_CODES } from '../constants/statusCodeConstants.js';
+import { s3Client } from '../config/default.js';
+
+export const uploadFileService = async (req, res) => {
+  await new Promise((resolve, reject) => {
+    upload.single("file")(req, res, (err) => {
+      if (err) {
+        return reject(
+          new APIError(STATUS_CODES.BAD_REQUEST, RESPONSE_MESSAGES.ERROR.INVALID_FILE_TYPE)
+        );
+      }
+      resolve();
+    });
+  });
+
+  const user_id = req.user?.user_id;
+  if (!user_id) {
+    throw new APIError(STATUS_CODES.BAD_REQUEST, RESPONSE_MESSAGES.ERROR.USER_NOT_FOUND);
   }
-  return user;
+
+  if (!req.body.document_type) {
+    throw new APIError(STATUS_CODES.BAD_REQUEST, RESPONSE_MESSAGES.ERROR.DOCUMENT_TYPE_NOT_FOUND);
+  }
+
+  if (!req.file) {
+    throw new APIError(STATUS_CODES.BAD_REQUEST, RESPONSE_MESSAGES.ERROR.FILE_NOT_FOUND);
+  }
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `uploads/${Date.now()}-${req.file.originalname}`,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  };
+
+  const command = new PutObjectCommand(params);
+
+  const [uploadFileResult, savedFile] = await Promise.all([
+    s3Client.send(command),
+    prisma.supporting_documents.create({
+      data: {
+        document_type: req.body.document_type,
+        document_name: req.file.originalname,
+        document_path: params.Key,
+        created_by: user_id,
+        status: "Draft",
+      },
+    }),
+  ]);
+
+  if (!uploadFileResult || uploadFileResult.$metadata.httpStatusCode !== STATUS_CODES.OK) {
+    throw new APIError(STATUS_CODES.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGES.ERROR.FILE_UPLOAD_FAILED);
+  }
+
+  return savedFile;
 };
